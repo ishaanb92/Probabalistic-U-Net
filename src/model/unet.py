ea385a9ad32d1b5f68d1e2af5f9d4b24bdd20611
kilgore92 <ishaan.bits92@gmail.com>
2019-04-29 15:54:25 +0200

    Adding a softmax layer to ensure predicted seg-map is a valid distribution over classes

--- src/model/unet.py
+++ src/model/unet.py
@@ -69,6 +69,10 @@ class UNet(nn.Module):
         #Output Layer
         self.output = nn.Conv2d(in_channels=int(self.enc_layer_depths[0]),out_channels= (self.n_classes + 1), kernel_size=1)
 
+        # Softmax Layer to create a seg-map that is a probability distribution
+        # over the different classes at each spatial location (So, dim=1 is used)
+        self.norm_layer = nn.Softmax(dim=1)
+
     def forward(self,x):
         #Encoder
         enc_outputs = []
@@ -90,7 +94,9 @@ class UNet(nn.Module):
         #Interpolate to match the size of seg-map
         out = F.interpolate(input=x,size=self.output_shape,mode='bilinear',align_corners=True)
 
-        return F.sigmoid(out)
+        softmax_out = self.norm_layer(out)
+
+        return softmax_out
 
 
 
